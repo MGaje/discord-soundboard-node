d061090ddae2e5e5b4c3ab03171899a0ba8a0791
MGajewski <gajewski.mike@gmail.com>
2018-03-16 15:54:11 -0400

    The soundboard connects to the voice channel specified in the config and plays an audio file.

--- src/core/Config.ts
+++ src/core/Config.ts
@@ -4,4 +4,5 @@
 export interface Config
 {
     botToken: string;
+    voiceChannel: string;
 }
--- src/core/Soundboard.ts
+++ src/core/Soundboard.ts
@@ -1,3 +1,5 @@
+import * as path from "path";
+
 import * as Discord from "discord.js"
 import * as Winston from "winston";
 
@@ -8,6 +10,8 @@ import { CommandHandler } from "../handlers/CommandHandler/CommandHandler";
 export class Soundboard
 {
     private botClient: Discord.Client;
+    private voiceConnection: Discord.VoiceConnection;
+    private isVoiceConnected: boolean;
     private config: Config;
     private dataStore: DataStore;
     private commandHandler: CommandHandler;
@@ -23,6 +27,7 @@ export class Soundboard
         this.config = require("../../config.json");
         this.dataStore = new DataStore();
         this.commandHandler = new CommandHandler(this.dataStore);
+        this.isVoiceConnected = false;
     }
 
     /**
@@ -45,7 +50,9 @@ export class Soundboard
         // Upon successful Discord connection.
         this.botClient.on("ready", () =>
         {
-            Winston.info("Connected to Discord.");
+            Winston.info("Connected to Discord. Ready.");
+            Winston.info("Attempting to join voice channel.");
+            this.connectToVoice();
         });
 
         // Open user input stream in the console.
@@ -55,5 +62,53 @@ export class Soundboard
             const input: string = d.toString().trim();
             this.commandHandler.handle({ command: input, discordClient: this.botClient, stdin: this.stdin });
         });
+    }
+
+    /**
+     * Connect to the voice channel as specified in the config.
+     */
+    private async connectToVoice(): Promise<void>
+    {
+        const voiceChannel: Discord.VoiceChannel = this.botClient.channels.find(x => x.id === this.config.voiceChannel) as Discord.VoiceChannel;
+        if (!voiceChannel)
+        {
+            Winston.error(`Cannot find voice channel with id ${this.config.voiceChannel}`);
+            throw new Error("Cannot find specified voice channel.");
+        }
+
+        this.voiceConnection = await voiceChannel.join();
+        this.voiceConnection.on("debug", msg => 
+        {
+            Winston.debug(`[Voice Connection Debug] ${msg}`);
+        });
+        this.voiceConnection.on("disconnect", () =>
+        {
+            Winston.info("Disconnected from the voice channel.");
+            this.voiceConnection.removeAllListeners();
+            this.voiceConnection.disconnect();
+            this.voiceConnection = null;
+            this.isVoiceConnected = false;
+        });
+        this.voiceConnection.on("error", e =>
+        {
+            Winston.error(`Voice connection error: ${e.message}`);
+            this.voiceConnection.removeAllListeners();
+            this.voiceConnection.disconnect();
+            this.voiceConnection = null;
+            this.isVoiceConnected = false;
+        });
+        this.voiceConnection.on("failed", e =>
+        {
+            Winston.error(`Failed to connect to voice: ${e.message}`);
+            this.voiceConnection.removeAllListeners();
+            this.voiceConnection.disconnect();
+            this.voiceConnection = null;
+            this.isVoiceConnected = false;
+        });
+
+        const effectsPath: string = path.resolve(__dirname, "../../effects");
+        const effectFile: string = path.join(effectsPath, "boop.wav");
+
+        const d: any = this.voiceConnection.playFile(effectFile);
     }
 }
